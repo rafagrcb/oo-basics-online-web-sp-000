385f9f01b987eca614197072c168dccea5471943
Rafael Garcia <rafagrcb@gmail.com>
2020-06-25 19:00:54 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -23,5 +23,17 @@ class Book
     @page_count
   end
   
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
 end
 
